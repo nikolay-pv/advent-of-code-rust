d55acd62962d5d0097630956d7f11e8ebe33ef1e
Nikolay Petrov <nikolay.a.petrov@gmail.com>
2022-12-04 15:44:05 +0100

    2022: day-03 optimize using bitmasks

--- 2022/day-03/src/main.rs
+++ 2022/day-03/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 const INPUT_TXT: &str = include_str!("input.txt");
 
 fn main() {
@@ -8,38 +6,44 @@ fn main() {
     println!("Answer to second parts is {}", solve_second(&input));
 }
 
-fn get_score(c: char) -> i32 {
+// gets index for the given char, where 'a' has index of 1 and 'A' has index of 27, etc.
+fn get_index(c: char) -> i32 {
     if 'a' <= c && c <= 'z' {
         return c as i32 - 'a' as i32 + 1;
     } else if 'A' <= c && c <= 'z' {
         return c as i32 - 'A' as i32 + 27;
     }
-    panic!("unexpected char in get_score")
+    panic!("unexpected char in get_index")
+}
+
+// gets index of the set bit, assuming only one bit is set, otherwise will return the highest set bit in mask
+fn get_setbit_index(mut mask: u64) -> i32 {
+    let mut counter = 0;
+    while mask != 1 {
+        mask >>= 1;
+        counter += 1;
+    }
+    return counter;
 }
 
 fn solve_first(input: &Vec<&str>) -> i32 {
     let mut sum = 0;
+    let mut seen = 0u64;
     for line in input {
         assert!(line.len() % 2 == 0);
-        let m = HashSet::<char>::from_iter(line[..line.len()/2].chars().into_iter());
+        // set the bits for the first part
+        line[..line.len()/2].chars().for_each(|c| set_bit(&mut seen, get_index(c)));
+        // search for duplicate
         for c in line[line.len()/2..].chars() {
-            if m.contains(&c) {
-                sum += get_score(c);
+            let idx = get_index(c);
+            if is_bit_set(seen, idx) {
+                sum += idx;
                 break;
             }
         }
+        seen = 0;
     }
     return sum;
-}
-
-fn get_intersection_score(seen: &[u64; 3]) -> i32 {
-    let mut common = seen[0] & seen[1] & seen[2];
-    let mut counter = 0;
-    while common != 1 {
-        common = common >> 1;
-        counter += 1;
-    }
-    return counter;
 }
 
 fn solve_second(input: &Vec<&str>) -> i32 {
@@ -48,13 +52,13 @@ fn solve_second(input: &Vec<&str>) -> i32 {
     let mut seen = [0u64; 3];
     for (i, line) in input.iter().enumerate() {
         let idx = i % 3;
-        for c in line.chars() {
-            // set bit corresponding to the found char
-            seen[idx] |= 1 << get_score(c);
-        }
+        // set bits for this line
+        line.chars().for_each(|c| set_bit(&mut seen[idx], get_index(c)));
         if idx == 2 {
-            sum += get_intersection_score(&seen);
-            seen = [0u64,0u64,0u64];
+            // mask holds common item across three strings
+            let mask = seen[0] & seen[1] & seen[2];
+            sum += get_setbit_index(mask);
+            seen = [0, 0, 0];
         }
     }
     return sum;
@@ -62,6 +66,16 @@ fn solve_second(input: &Vec<&str>) -> i32 {
 
 fn read_input(file_content: &str) -> Vec<&str> {
     file_content.lines().into_iter().collect()
+}
+
+#[inline(always)]
+fn set_bit(mask: &mut u64, n: i32) {
+    *mask |= 1 << n;
+}
+
+#[inline(always)]
+fn is_bit_set(mask: u64, n: i32) -> bool {
+    (mask & (1 << n)) != 0
 }
 
 
