91a37924c6e32b363c77bf33a5b0619328c931cb
Nikolay Petrov <nikolay.a.petrov@gmail.com>
2022-12-19 22:18:54 +0100

    2022: day-09 solution for the second part

--- 2022/day-09/src/main.rs
+++ 2022/day-09/src/main.rs
@@ -21,53 +21,65 @@ impl Point {
 #[derive(Debug)]
 struct Grid {
     trace: HashSet<Point>,
-    head: Point,
-    tail: Point,
+    nodes: Vec<Point>,
 }
 
 impl Grid {
-    fn new() -> Grid {
+    fn new(size: usize) -> Grid {
         Grid{
             trace: HashSet::from([Point::zero()]),
-            head: Point::zero(),
-            tail: Point::zero(),
+            nodes: vec![Point::zero(); size],
         }
     }
 
-    fn move_head(&mut self, mv: &Point) {
-        self.head.0 += mv.0;
-        self.head.1 += mv.1;
+    fn head(&mut self) -> &mut Point {
+        self.nodes.first_mut().unwrap()
+    }
+
+    fn tail(&mut self) -> &mut Point {
+        self.nodes.last_mut().unwrap()
+    }
+
+    fn move_head(&mut self, mut mv: Point) {
+        while mv != Point::zero() {
+            let d = Point(mv.0.signum(), mv.1.signum());
+            self.head().0 += d.0;
+            self.head().1 += d.1;
+            self.move_tail();
+            mv.0 -= d.0;
+            mv.1 -= d.1;
+        }
     }
 
     fn move_tail(&mut self) {
-        let dx = self.head.0 - self.tail.0;
-        let dy = self.head.1 - self.tail.1;
-        if dx.abs() <= 1 && dy.abs() <= 1 {
-            return;
+        for i in 1..self.nodes.len() {
+            let dx = self.nodes[i-1].0 - self.nodes[i].0;
+            let dy = self.nodes[i-1].1 - self.nodes[i].1;
+            if dx.abs() <= 1 && dy.abs() <= 1 {
+                continue;
+            }
+            self.nodes[i].0 += dx.signum();
+            self.nodes[i].1 += dy.signum();
         }
-        self.tail.0 += dx.signum();
-        self.tail.1 += dy.signum();
-        self.trace.insert(self.tail);
+        let tail = *self.tail();
+        self.trace.insert(tail);
     }
 }
 
 fn solve_first(input: &Vec<Point>) -> usize {
-    println!("");
-    let mut g = Grid::new();
-    for mut mv in input.iter().copied() {
-        while mv != Point::zero() {
-            let d = Point(mv.0.signum(), mv.1.signum());
-            g.move_head(&d);
-            g.move_tail();
-            mv.0 -= d.0;
-            mv.1 -= d.1;
-        }
+    let mut g = Grid::new(2);
+    for mv in input.iter() {
+        g.move_head(*mv);
     }
     return g.trace.len();
 }
 
 fn solve_second(input: &Vec<Point>) -> usize {
-    return input.len();
+    let mut g = Grid::new(10);
+    for mv in input.iter() {
+        g.move_head(*mv);
+    }
+    return g.trace.len();
 }
 
 fn to_coord_increment(line: &str) -> Point {
@@ -92,18 +104,31 @@ fn read_input(file_content: &str) -> Vec<Point> {
 #[cfg(test)]
 mod tests {
     use crate::*;
-    const TEST_INPUT_TXT: &str = include_str!("input.txt");
+    const INPUT_TXT: &str = include_str!("input.txt");
+    const TEST_INPUT_TXT: &str = include_str!("input_test.txt");
+    const TEST_INPUT_PART2_TXT: &str = include_str!("input_test_part2.txt");
+
+    #[test]
+    fn movement() {
+        let mut g = Grid::new(2);
+        let mv = Point(4, 0);
+        g.move_head(mv);
+        assert_eq!(*g.head(), Point(4,0));
+        assert_eq!(*g.tail(), Point(3,0));
+    }
 
     #[test]
     fn part1() {
         let input = read_input(TEST_INPUT_TXT);
         assert_eq!(solve_first(&input), 13);
+        let input = read_input(INPUT_TXT);
+        assert_eq!(solve_first(&input), 6011);
     }
 
     #[test]
     fn part2() {
-        let input = read_input(TEST_INPUT_TXT);
-        assert_eq!(solve_second(&input), 70);
+        let input = read_input(TEST_INPUT_PART2_TXT);
+        assert_eq!(solve_second(&input), 36);
     }
 }
 
